eff2e78bc5ff206bd02c0a1c87a3624c997865f6
yuvarajnineleaps <yuvaraj@nineleaps.com>
2017-05-12 14:08:10 +0530

    Added Base working Web api & swagger for same

---
+++ Ads/Controllers/AdsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Ads.Models;
+
+namespace Ads.Controllers
+{
+    public class AdsController : ApiController
+    {
+        Ad[] ads = new Ad[]
+        {
+            new Ad {Id = 1, Name = "Android", Priority=1 },
+            new Ad {Id = 2, Name = "Web", Priority=3 },
+            new Ad {Id = 3, Name = "iOS", Priority=2 }
+        };  
+        
+        public IEnumerable<Ad> GetAllAds()
+        {
+            return ads;
+        }
+
+        public IHttpActionResult GetAd(int id)
+        {
+            var advertise = ads.FirstOrDefault((p) => p.Id == id);
+            if (advertise == null)
+            {
+                return NotFound();
+            }
+            return Ok(advertise);
+        }
+    }
+}
