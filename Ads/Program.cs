586bb2766573a58da0b3cb094871faa7fab83aba
yuvarajnineleaps <yuvaraj@nineleaps.com>
2017-05-18 14:11:55 +0530

    Added self host configuration and changed output type as console in properties

---
+++ Ads/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin.Hosting;
+using System.Threading;
+
+namespace Ads
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://localhost:8080"))
+            {
+                Console.WriteLine("Web Server is running.");
+                Thread.Sleep(Timeout.Infinite);
+                Console.WriteLine("Press any key to quit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Ads/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Owin;
+using System.Web.Http;
+
+namespace Ads
+{
+    public class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+
+            // Swagger
+            SwaggerConfig.Register(config);
+
+            // Authentication token
+            // ConfigureOAuth(app);
+
+            // SignalR configuration
+            // ConfigureSignalR(app);
+
+            // Register routes
+            WebApiConfig.Register(config);
+
+            // Allow cross-domain requests
+            // appBuilder.UseCors(CorsOptions.AllowAll);
+
+            appBuilder.UseWebApi(config);
+
+
+
+        }
+    }
+}
